120f4de23f3c6aec14c159f49f35bee1abaf2155
Emmanuelitgit <eyidana001@st.ug.edu.gh>
2025-08-09 17:35:17 +0000

    added swagger configurations

--- src/main/java/springKeycloak/config/SwaggerConfig.java
+++ src/main/java/springKeycloak/config/SwaggerConfig.java
@@ -5,8 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,18 +24,36 @@ public class SwaggerConfig {
                 .scheme("bearer");
     }
 
+//    @Bean
+//    public OpenAPI openAPI() {
+//        return new OpenAPI()
+//                .addSecurityItem(new SecurityRequirement().
+//                        addList("Bearer Authentication"))
+//                .components(new Components().addSecuritySchemes
+//                        ("Bearer Authentication", createAPIKeyScheme()))
+//                .info(new Info().title("Functional Based Permission")
+//                        .description("Spring Integration API.")
+//                        .version("1.0").contact(new Contact().name("Code With Manuel Dev")
+//                                .email( "www.manueldev.com").url("eyidana001@gmail.com"))
+//                        .license(new License().name("License of API")
+//                                .url("API license URL")));
+//    }
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement().
-                        addList("Bearer Authentication"))
-                .components(new Components().addSecuritySchemes
-                        ("Bearer Authentication", createAPIKeyScheme()))
-                .info(new Info().title("Functional Based Permission")
-                        .description("Spring Integration API.")
-                        .version("1.0").contact(new Contact().name("Code With Manuel Dev")
-                                .email( "www.manueldev.com").url("eyidana001@gmail.com"))
-                        .license(new License().name("License of API")
-                                .url("API license URL")));
+                .components(new Components()
+                        .addSecuritySchemes("keycloak", new SecurityScheme()
+                                .type(SecurityScheme.Type.OAUTH2)
+                                .flows(new OAuthFlows()
+                                        .password(new OAuthFlow()
+                                                .tokenUrl("http://localhost:8080/realms/TestRealm/protocol/openid-connect/token")
+                                                .scopes(new Scopes().addString("openid", "OpenID Connect scope"))
+                                        )
+                                )
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("keycloak"))
+                .info(new Info().title("My API").version("1.0"));
     }
 }
